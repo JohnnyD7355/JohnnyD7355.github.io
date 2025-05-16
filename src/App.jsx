48de06b451c74ab474cf8033a2483b3f88f340b4
Johnny Dang <jdang384@west-mec.org>
2025-05-16 16:40:29 -0700

    5/16

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { LoadingScreen } from './components/LoadingScreen';
 import { Navbar } from './components/Navbar';
 import "./index.css";
 import {useState} from "react";
+import { MobileMenu } from './components/MobileMenu';
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -13,6 +14,7 @@ function App() {
     {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}{" "}
       <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
       <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       </div>
     
     </>
