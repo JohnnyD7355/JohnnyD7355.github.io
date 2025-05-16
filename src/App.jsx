ecebd3a965331fefb73bab8bd42d1bb5d4b2430f
Johnny Dang <jdang384@west-mec.org>
2025-05-15 17:27:21 -0700

    5/21

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,14 @@ import "./index.css";
 import {useState} from "react";
 
 function App() {
-  const [isLoaded, setIsLoaded] = useState(false)
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <>
     {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}{" "}
       <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
-      <Navbar />
+      <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       </div>
     
     </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,13 @@
+import {useEffect} from 'react';
 
-export const Navbar = () => {
+export const Navbar = ({menuOpen, setMenuOpen}) => {
 
-    return <nav className="fixed top-0 w-full z-40 bg-[rgba(10, 10, 0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">{" "}
+    useEffect(() => {
+        document.body.style.overflow = menuOpen ? "hidden" : "";
+    }, [menuOpen]);
+
+    return (
+     <nav className="fixed top-0 w-full z-40 bg-[rgba(10, 10, 0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">{" "}
         <div className="max-width-5xl mx-auto px-4">
             <div className="flex justify-between items-center h-16">
                 <a href="#home" className="font-mono text-xl font-bold text-white">
@@ -9,14 +15,15 @@ export const Navbar = () => {
                     johnny<span className="text-blue-500">.tech</span>{" "}
                 </a>
 
-                <div className="w-7 h-5 relative cursor-pointer z-40 md:hidden">
+                <div className="w-7 h-5 relative cursor-pointer z-40 md:hidden" 
+                onClick ={() => setMenuOpen((prev) => !prev)}>
                 &#9776;
                 </div>
 
                 <div className="hidden md:flex items-center space-x-8">
                     <a href="#home" className="text-gray-300 hover:text-white transition-colors"> {" "}Home{" "}</a>
                     <a href="#about" className="text-gray-300 hover:text-white transition-colors"> {" "}About{" "} </a>
-                    <a href="#projects" className="text-gray-300 hover:text-white transition-colors"> {" "}Projects{" "} </a>
+                    <a href="#projects" className="text-gra~y-300 hover:text-white transition-colors"> {" "}Projects{" "} </a>
                     <a href="#contact" className="text-gray-300 hover:text-white transition-colors"> {" "}Contact{" "} </a>
                 </div>
 
@@ -25,4 +32,5 @@ export const Navbar = () => {
             </div>
         </div>
     </nav>
+    )
 }
