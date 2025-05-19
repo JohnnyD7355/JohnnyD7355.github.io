73dbba439cb016168cbe42d8ad9d9e23c6432432
Johnny Dang <JohnnyD7355@gmail.com>
2025-05-19 00:36:42 -0700

    12:36am

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -13,7 +13,7 @@ export const About = () => {
                 Passionate coder with expertise in front-end development and UI/UX Design.
             </p>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 gaap-6">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                 <div className="rounded-xl p-6 hover:-translate-y-1 transtion-all">
                     <h3 className="text-xl font-bold mb-4"> Frontend</h3>
                     <div className="flex flex-wrap gap-2">
@@ -27,8 +27,39 @@ export const About = () => {
                         ))}
                     </div>
                 </div>
+
+                 <div className="rounded-xl p-6 hover:-translate-y-1 transtion-all">
+                    <h3 className="text-xl font-bold mb-4"> Backend</h3>
+                    <div className="flex flex-wrap gap-2">
+                        {backendSkills.map((tech, key) => (
+                            <span 
+                            key={key}
+                            className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm 
+                            hover:bg-blue-500/20 hover:shadow-[0_2px_80x_rgba(59,130,2246,0.2)] transition">
+                                {tech}
+                            </span>
+                        ))}
+                    </div>
+                </div>
+
             </div>
         </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
+            <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4"> Education</h3>
+            </div>
+            <ul>
+                <li>
+                    <strong> Certified in HTML5 Application Development and JavaScript</strong>
+                </li>
+            </ul>
+            <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4"> Work Experience</h3>
+            </div>
+            
+        </div>
+
         </div>
     </section>;
-}
+};
